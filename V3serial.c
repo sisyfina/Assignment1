c0f1abc77946dbe96a66e926a1bf7f02ac66436e
sisyfina <45711984+sisyfina@users.noreply.github.com>
2020-12-05 23:30:24 +0200

    Update and rename PreliminariesV3serial.c to V3serial.c

--- V3serial.c
+++ V3serial.c
@@ -18,16 +18,16 @@ void coo2csc(
   uint32_t const         isOneBased /*!< Whether COO is 0- or 1-based */
 );
 
-int binarySearch(int *vector, int start, int end, int value)
+uint32_t binarySearch(uint32_t *vector, uint32_t start, uint32_t end, uint32_t value)
 {
-  int numElem = end - start + 1;
-  int i = start;
+  uint32_t numElem = end - start + 1;
+  uint32_t i = start;
 
   if(numElem < 1)
     return 1;
   else if(numElem<SERIAL_ELEMENT_LIMIT)
   {
-    for(int tmp=0; tmp<numElem; tmp++)
+    for(uint32_t tmp=0; tmp<numElem; tmp++)
     {
       if(value == vector[start+tmp])
         return 0;
@@ -62,9 +62,9 @@ int main(int argc, char *argv[] ) {
 	int ret_code;
 	MM_typecode matcode;
 	FILE *f;
-	int M, N, nz, NNZ;
-	int *I, *J;
-	int *val;
+	uint32_t M, N, nz, NNZ;
+	uint32_t *I, *J;
+	uint32_t *val;
 
 /*
 *	2 arguments for now
@@ -119,16 +119,16 @@ int main(int argc, char *argv[] ) {
 
 	/* reseve memory for matrices */
 
-	I = (int *) malloc(nz * sizeof(int));
-	J = (int *) malloc(nz * sizeof(int));
-	val = (int *) malloc(nz * sizeof(int));
+	I = (uint32_t *) malloc(nz * sizeof(uint32_t));
+	J = (uint32_t *) malloc(nz * sizeof(uint32_t));
+	val = (uint32_t *) malloc(nz * sizeof(uint32_t));
 
 	/* Replace missing or double val column with 1s and change the fscanf to match pattern matrices*/
 
-	int flag = 0;
-	for (int i=0; i<nz; i++)
+	uint32_t flag = 0;
+	for (uint32_t i=0; i<nz; i++)
 	{
-		fscanf(f, "%d %d\n", &I[flag], &J[flag]);
+		fscanf(f, "%u %u\n", &I[flag], &J[flag]);
 		if(I[flag] != J[flag])
 		{
       val[flag]=1;
@@ -154,16 +154,16 @@ int main(int argc, char *argv[] ) {
 
 	// CSR format equal to CSC
 
-	int *V = (int *)malloc(NNZ * sizeof(int));
-	int *ROW_INDEX = (int *)malloc(NNZ * sizeof(int));
-	int *COL_INDEX = (int *)malloc((N+1) * sizeof(int));
+	int *V = (uint32_t *)malloc(NNZ * sizeof(uint32_t));
+	int *ROW_INDEX = (uint32_t *)malloc(NNZ * sizeof(uint32_t));
+	int *COL_INDEX = (uint32_t *)malloc((N+1) * sizeof(uint32_t));
 	COL_INDEX[0] = 0;
 	COL_INDEX[N] = NNZ;
 
 	coo2csc(ROW_INDEX, COL_INDEX, I, J, NNZ, N, 0);
-
+/*
 	printf("V =");
-	for(int i = 0; i<NNZ; i++)
+	for(uint32_t i = 0; i<NNZ; i++)
   {
 		V[i] = 1;
 		printf(" %d", V[i]);
@@ -171,71 +171,71 @@ int main(int argc, char *argv[] ) {
 	printf("\n");
 
 	printf("ROW_INDEX =");
-	for(int i = 0; i<NNZ; i++)
+	for(uint32_t i = 0; i<NNZ; i++)
   {
 		printf(" %d", ROW_INDEX[i]);
 	}
 	printf("\n");
 
 	printf("COL_INDEX =");
-	for(int i = 0; i<N+1; i++)
+	for(uint32_t i = 0; i<N+1; i++)
   {
 		printf(" %d", COL_INDEX[i]);
 	}
 	printf("\n");
-
+*/
 	// vector with number of triangles that corresponds to a node
-	int *c3 = (int *)malloc(N * sizeof(int *));
+	uint32_t *c3 = (uint32_t *)malloc(N * sizeof(uint32_t *));
 
 	// initialize c3
-	for(int i=0; i<N; i++)
+	for(uint32_t i=0; i<N; i++)
 		c3[i] = 0;
 
   //int *edgeNum = (int *)malloc(N * sizeof(int *));
 
-  for(int i=0; i<N-1; i++)
+  for(uint32_t i=0; i<N-1; i++)
   {
-    printf("i = %d\n", i);
-    int ci_next = COL_INDEX[i+1];
-    int ci = COL_INDEX[i];
-    int iEdgeNum = ci_next - ci;
-    if(iEdgeNum>0)
-      int *cineigh = (int *)malloc(iEdgeNum * sizeof(int *));
+    //printf("i = %u\n", i);
+    uint32_t ci_next = COL_INDEX[i+1];
+    uint32_t ci = COL_INDEX[i];
+    uint32_t iEdgeNum = ci_next - ci;
+    if(iEdgeNum>0){
+    uint32_t *cineigh = (int *)malloc(iEdgeNum * sizeof(int *));
     // find i's neighbours
-    printf("cineigh =");
-    for(int flag = 0; flag<iEdgeNum; flag++)
+    //printf("cineigh =");
+    for(uint32_t flag = 0; flag<iEdgeNum; flag++)
     {
       cineigh[flag] = ROW_INDEX[ci+flag];
-      printf(" %d", cineigh[flag]);
+      //printf(" %u", cineigh[flag]);
     }
-    printf("\n");
+    //printf("\n");
 
     // search by edge
-    for(int j=0; j<iEdgeNum; j++)
+    for(uint32_t j=0; j<iEdgeNum; j++)
     {
-      int jneigh = cineigh[j];
-      printf("j = %d\n", jneigh);
-      int cj_next = COL_INDEX[jneigh+1];
-      int cj = COL_INDEX[jneigh];
-      int jEdgeNum = cj_next - cj;
-      if(jEdgeNum>0)
-        int *cjneigh = (int *)malloc(jEdgeNum * sizeof(int *));
+      uint32_t jneigh = cineigh[j];
+      //printf("j = %u\n", jneigh);
+      uint32_t cj_next = COL_INDEX[jneigh+1];
+      uint32_t cj = COL_INDEX[jneigh];
+      uint32_t jEdgeNum = cj_next - cj;
+      if(jEdgeNum>0){
+      uint32_t *cjneigh = (uint32_t *)malloc(jEdgeNum * sizeof(uint32_t *));
       // find j's neighbours
-      printf("cjneigh =");
-      for(int flag = 0; flag<jEdgeNum; flag++)
+      //printf("cjneigh =");
+      for(uint32_t flag = 0; flag<jEdgeNum; flag++)
       {
         cjneigh[flag] = ROW_INDEX[cj+flag];
-        printf(" %d", cjneigh[flag]);
+        //printf(" %d", cjneigh[flag]);
       }
-      printf("\n");
+      //printf("\n");
 
-      for(int k=0; k<jEdgeNum; k++)
+      for(uint32_t k=0; k<jEdgeNum; k++)
       {
-        int kneigh = cjneigh[k];
-        printf("k = %d\n", kneigh);
+        uint32_t kneigh = cjneigh[k];
+        //printf("k = %u\n", kneigh);
 
-        int tmp = binarySearch(cineigh, 0, iEdgeNum-1, kneigh);
-        printf("tmp = %d\n", tmp);
+        uint32_t tmp = binarySearch(cineigh, 0, iEdgeNum-1, kneigh);
+        //printf("tmp = %u\n", tmp);
         if(tmp == 0)
         {
           c3[i]++;
@@ -247,14 +247,14 @@ int main(int argc, char *argv[] ) {
       }
     }
 
-
+    }
   }
-
+/*
   printf("c3 =");
-	for(int i = 0; i<N; i++) {
+	for(uint32_t i = 0; i<N; i++) {
 		printf(" %d", c3[i]);
 	}
 	printf("\n");
-
-
+  }
+*/
 }
